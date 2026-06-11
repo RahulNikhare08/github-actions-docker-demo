593d2c936cbc245a2b5f9cd452ca120be9be6fae
Nikhare <rahul.l.nikhare@accenture.com>
2026-06-11 16:53:33 +0530

    Version 3

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from GitHub Actions! Verrion2"
+    return "Hello from GitHub Actions! Verrion3"
     
 app.run(host="0.0.0.0", port=5000)
